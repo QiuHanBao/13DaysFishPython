1497e9828764f3c63ccda76f699e794bff246a2e
qiuhbhome <qiuhanbao@outlook.com>
2019-12-18 20:48:09 +0800

    第二次推送

--- day06/bian.py
+++ day06/bian.py
@@ -12,8 +12,9 @@ response.encoding = 'gbk'
 # print(response.text)
 e = html.etree.HTML(response.text)
 # 获得每一期数据
-types = e.xpath('//div[@id="main"]/div[@class="classify clearfix"]/a/text()')
-names = e.xpath('//div/div/ul/li/a/b/text()')
+names = e.xpath('//div/ul/li/a/b/text()')
+# types = e.xpath('//div/div/ul/li/a/b/text()')
+type = '4K动漫'
 srcvs = e.xpath('//div/div/ul/li/a/img/@src')
 srcs = e.xpath('//div/div/ul[@class="clearfix"]/li/a/@href')
 baseurl = 'http://pic.netbian.com'
@@ -21,7 +22,9 @@ baseurl = 'http://pic.netbian.com'
 client = pymysql.connect(host='localhost', port=3306, user='root', password='111111', charset='utf8', db='djmysql')
 cursor = client.cursor()
 insql = 'insert into blog_bian  values (0,%s,%s,%s,%s)'
-for type, name, srcv, src in zip(types, names, srcvs, srcs):
+for name, srcv, src in zip(names, srcvs, srcs):
+    print(name)
+    # print(type)
     srcv = baseurl + srcv
     src = baseurl + src
     cursor.execute(insql, [type, name, srcv, src])
